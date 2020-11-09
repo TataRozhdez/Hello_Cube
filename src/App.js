14d98992499e41ed32414d78355aead177766f22
Tata Rozhdez <tanya.zhukova.95@gmail.com>
2020-11-09 16:27:47 +0200

    cube animate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react'
+import { Canvas, useThree, extend, useFrame } from 'react-three-fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { a, useSpring } from 'react-spring/three'
+import './App.css'
+
+extend({ OrbitControls })
+
+function Cube(props) {
+  const [isBig, setIsBig] = useState(false)
+  const [isHovered, setIsHovered] = useState(false)
+  const ref = useRef()
+
+  useFrame(() => {
+    ref.current.rotation.x += 0.01
+    ref.current.rotation.y += 0.01
+  })
+
+  const { size, x } = useSpring({
+    size: isBig ? [2, 2, 2] : [1, 1, 1],
+    x: isBig ? 2 : 0,
+  })
+
+  const color = isHovered ? 'pink' : 'salmon'
+
+  return (
+    <a.mesh
+      {...props}
+      ref={ref}
+      scale={size}
+      position-x={x}
+      onClick={() => setIsBig(!isBig)}
+      onPointerOut={() => setIsHovered(false)}
+      onPointerOver={() => setIsHovered(true)}
+    >
+      <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
+      <meshStandardMaterial attach='material' color={color} />
+    </a.mesh>
+  )
+}
+
+function Scene() {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree()
+
+  return (
+    <>
+      <ambientLight />
+      <pointLight intensity={0.9} position={[-1, 2, 4]} />
+      <Cube rotation={[10, 10, 0]} position={[0, 0, 0]} />
+      <Cube rotation={[10, 10, 0]} position={[2, 2, 0]} />
+      <orbitControls args={[camera, domElement]} />
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Canvas>
+      <Scene />
+    </Canvas>
+  )
 }
 
-export default App;
+export default App
